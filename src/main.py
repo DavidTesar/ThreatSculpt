ab200134789117be4c6598496010f40eaffd70a7
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2024-02-12 09:47:17 -0600

    moved all dependencies into one install script - /test/installModules.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,16 @@
 from scan import findHosts, findVulns
-from libs import *
-#from api import makeRequestPulseDive, makeRequestVuln
+from api import makeRequestPulseDive, makeRequestVuln
 # TODO: install libraries outside, first.
 
 def main():
     print("Checking for libraries...")
-    nmapLib()
-    vulnsLib()
-    apiLib()
 
     # This is where modules are called
     findHosts()
     findVulns()
-    apiLib()
 
-
-    #makeRequestPulseDive("zeus")
-    #makeRequestVuln("CVE_2017_14174")
+    makeRequestPulseDive("zeus")
+    makeRequestVuln("CVE_2017_14174")
 
 
 
