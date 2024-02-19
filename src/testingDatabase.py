523c64ac5c63a849a2de45a7f13a458ce86dba68
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2024-02-18 19:52:06 -0600

    Database updated, yet not all info is showing

--- src/testingDatabase.py
+++ src/testingDatabase.py
@@ -1,10 +1,25 @@
-import numpy as np 
 from pymongo.mongo_client import MongoClient
 from urllib.parse import quote_plus
 from scan import findHosts
+import hashlib
+import uuid
+
+def create_id(input_string):
+    # Hash the input string using SHA-256
+    hash_object = hashlib.md5(input_string.encode())
+    hash_hex = hash_object.hexdigest()
+    
+    
+    # Combine the hash and the UUID to create a unique scan ID
+    scan_id = f"{hash_hex}"
+    
+    print("ID: " + scan_id)
+
+    return scan_id
 
 username = quote_plus('user_test')
 password = quote_plus('CZ66ttLSf5s0GVe4')
+
 uri = "mongodb+srv://"+username+":"+password + "@cluster0.zc7grf3.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri)
@@ -19,28 +34,32 @@ result = []
 
 for host in scanResults.all_hosts():
     print("Host: ", host)
+    myHosts.append(host)
     
 
     #print("State: ", scanResults[host].state())
     for proto in scanResults[host].all_protocols():
         #print("Protocol: ", proto)
         ports = scanResults[host][proto].keys()
-        for port in ports:
-        #     print("Port: ", port, "State: ", scanResults[host][proto][port]['state'], "Version: ", scanResults[host][proto][port]['version'])
-            myNumber = port
-            myState = scanResults[host][proto][port]['state']
-            myType = proto
-            myPorts.append(ports)
-            myVersion = scanResults[host][proto][port]['version']
+        # for port in ports:
+        # #     print("Port: ", port, "State: ", scanResults[host][proto][port]['state'], "Version: ", scanResults[host][proto][port]['version'])
+        #     myNumber = port
+        #     myState = scanResults[host][proto][port]['state']
+            
+        #     myType = proto
+        #     myPorts.append(ports)
+        #     myVersion = scanResults[host][proto][port]['version']
 
-            portObject = {
-                "port_num" : myNumber,
-                "port_state" : myState,
-                "protocol" : myType,
-                "protocol_version" : myVersion
-            }
+        #     portObject = {
+        #         "port_num" : myNumber,
+        #         "port_state" : myState,
+        #         "protocol" : myType,
+        #         "protocol_version" : myVersion
+        #     }
 
-            myPorts.append(portObject)
+        #     myPorts.append(portObject)
+        myPorts.append(list(ports))
+
         
         myResult = {
             "host_num" : host,
@@ -55,13 +74,9 @@ try:
     dbname = client.ThreatSculpt
     col_name = dbname.ScanResults
 
-# TODO: hash of the output = id
-    scanID = "90234589"
-
-# TODO: hash of the hosts discovered = id
-    networkID = "80978966"
-
-    userID = "90878909"
+    scanID = create_id(str(scanResults))
+    networkID = create_id(str(myHosts))
+    userID = create_id(username)
 
     scan = {
         "userID" : userID,
@@ -73,4 +88,5 @@ try:
     col_name.insert_one(scan)
     print("it should work..")
 except Exception as e:
+    print("exception cought..")
     print(e)
