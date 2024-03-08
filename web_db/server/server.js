233f42c48f55e646eebddedf2f3fd917e7e2733d
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2024-03-08 10:41:01 -0600

    Add files via upload

--- web_db/server/server.js
+++ web_db/server/server.js
@@ -1,1 +1,33 @@
+import Express from 'express'
+import dataRouter from './dataRoutes.mjs'
+//import mongoose from 'mongoose'
+//import MongoStore from 'connect-mongo'
+//import { Mongo } from './mongoControllers.js'
+import path from 'path'
 
+//const store = new session.MemoryStore()
+path.__dirname = path.resolve(path.dirname('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public'))
+
+
+const PORT = 3000
+const app = new Express()
+
+app.use(Express.json())
+
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request at ${req.url}`)
+  next()
+})
+// Statically serve the public folder
+app.use(Express.static(path.join('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public/index.html')))
+
+app.use('/web/server', dataRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: 'C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
