6d4816d8492f2fa3a440206f3e99deb8afa0905e
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2024-03-09 01:55:13 -0600

    Add files via upload

--- web_db/server/server.js
+++ web_db/server/server.js
@@ -6,7 +6,7 @@ import dataRouter from './dataRoutes.mjs'
 import path from 'path'
 
 //const store = new session.MemoryStore()
-path.__dirname = path.resolve(path.dirname('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public'))
+path.__dirname = path.resolve(path.dirname('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web_db/public/index.html'))
 
 
 const PORT = 3000
@@ -20,12 +20,12 @@ app.use((req, res, next) => {
   next()
 })
 // Statically serve the public folder
-app.use(Express.static(path.join('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public/index.html')))
+app.use(Express.static(path.join('C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web_db/public')))
 
 app.use('/web/server', dataRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile('index.html', { root: 'C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web/public' })
+  res.sendFile('index.html', { root: 'C:/Users/TrinhAn/OneDrive - University of Wisconsin-Stout/Desktop/ThreatSculpt-main/web_db/public' })
 })
 
 app.listen(PORT, () => {
