f54b1df1b221cd32d3db04be11d5f6bc0c8c21f7
Massey <masseyc8061@my.uwstout.edu>
2024-03-29 11:06:56 -0500

    Changed paths

--- src/open_react_app.py
+++ src/open_react_app.py
@@ -2,11 +2,11 @@ import subprocess
 import os
 
 # Define the path to the React app directory
-react_app_path = r'..\web_db'
+react_app_path = r'..\website'
 
 # Define a function to start the React app
 def start_react_app():
-    subprocess.Popen('npm run server:prod', cwd=react_app_path, shell=True)
+    subprocess.Popen('npm start', cwd=react_app_path, shell=True)
 
 if __name__ == '__main__':
     start_react_app()
